00eda17d823d4f073c05d3a80fcf40426a55ff00
Levan Madurashvili <55601009+maduraaa@users.noreply.github.com>
2022-02-26 22:00:27 +0400

    add page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contact from './pages/Contact';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import NotFound from './pages/NotFound';
+import Layout from './layout/Layout';
 
-function App() {
+const App = () => {
+
   return (
-    <div className="App">
-      levan
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout><Dashboard /></Layout>} />
+        <Route path="/projects" element={<Layout><Projects /></Layout>} />
+        <Route path="/contact" element={<Layout><Contact /> </Layout>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
