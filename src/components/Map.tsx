9aa2b94bd589fe4151e3c6d5ad299b9613cacc5b
Levan Madurashvili <55601009+maduraaa@users.noreply.github.com>
2022-03-02 20:10:43 +0400

    add custom google map

---
+++ src/components/Map.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components';
+import { GoogleMap, withScriptjs, withGoogleMap, Marker } from "react-google-maps"
+import mapStyles from '../data/mapStyles';
+import MapMarkerIcon from '../icon/map-marker.png'
+
+
+
+const MapSetting: React.FC<{}> = ({ }) => {
+  return (
+    <GoogleMap
+      defaultZoom={13} // default map zoom
+      defaultCenter={{ lat: 41.720287, lng: 44.740616 }} //location
+      defaultOptions={{ styles: mapStyles }} //custom map json
+    >
+      <Marker
+        position={{ lat: 41.720287, lng: 44.740616 }} //custom location
+        icon={{
+          url: `${MapMarkerIcon}`, // custom map marker
+          scaledSize: { width: 32, height: 32 } //change size
+        }}
+      />
+    </GoogleMap>
+  )
+};
+
+const WrappedMap = withScriptjs(withGoogleMap(MapSetting)); //exported full map
+
+const Map = () => {
+  return (
+    <Wrapper>
+      <WrappedMap googleMapURL={'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyA4IQUYzP_Sb61zJwS8GKSKnmjoYxJkOC4'} //api url and key
+        loadingElement={<div style={{ height: '100%' }}></div>} //needed for height
+        containerElement={<div style={{ height: '100%' }}></div>} //needed for height
+        mapElement={<div style={{ height: '100%' }}></div>} //needed for height
+      />
+    </Wrapper>
+  )
+}
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+export default Map
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
 import styled from 'styled-components';
 import Responsive from '../tools/Responsive';
-
+import GoogleMap from '../components/Map';
+import { Container } from '../tools/GlobalStyle';
 
 const Contact = () => {
   return (
-    <ContactdWrapper>Contact</ContactdWrapper>
+    <Container>
+      <ContactdWrapper>
+        <MapWrapper>
+          {/* <div> next here </div> */}
+          <GoogleMap />
+        </MapWrapper>
+      </ContactdWrapper>
+    </Container>
   )
 }
 
@@ -13,12 +20,19 @@ const Contact = () => {
 const ContactdWrapper = styled.div`
   min-height: calc(100vh - 180px);
   display: flex;
-  justify-content: center;
-  align-items: center;
+  padding-top: 50px;
+  padding-bottom: 100px;
     ${Responsive.mobile} {
       height: auto;
       padding-top: 20px;
     }
+`;
+const MapWrapper = styled.div`
+  width: 100vw;
+  height: 400px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 export default Contact
