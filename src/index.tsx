64801f1866cb521633c82f65fa5abfb6788a6ae2
Levan Madurashvili <55601009+maduraaa@users.noreply.github.com>
2022-09-04 15:45:29 +0400

    added messenger

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { MessengerChat } from "react-messenger-chat-plugin";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <MessengerChat
+      pageId="106392365544446"
+      language="sv_SE"
+      themeColor={"#000000"}
+      bottomSpacing={300}
+      loggedInGreeting="loggedInGreeting"
+      loggedOutGreeting="loggedOutGreeting"
+      greetingDialogDisplay={"show"}
+      debugMode={true}
+      onMessengerShow={() => {
+        console.log("onMessengerShow");
+      }}
+      onMessengerHide={() => {
+        console.log("onMessengerHide");
+      }}
+      onMessengerDialogShow={() => {
+        console.log("onMessengerDialogShow");
+      }}
+      onMessengerDialogHide={() => {
+        console.log("onMessengerDialogHide");
+      }}
+      onMessengerMounted={() => {
+        console.log("onMessengerMounted");
+      }}
+      onMessengerLoad={() => {
+        console.log("onMessengerLoad");
+      }}
+    />
+    ,
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
